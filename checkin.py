721f1f5e9b056076c1c86b0b4739744a704a2d98
liuyq917 <38929869+liuyq917@users.noreply.github.com>
2020-11-23 11:48:25 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -15,14 +15,14 @@ requests.packages.urllib3.disable_warnings()
 class SspanelQd(object):
     def __init__(self):
         # 机场地址
-        self.base_url = input()
+        self.base_url = ['https://www.paofu.cloud','https://youyun777.net','https://sakura.kozow.com','https://www.paofu.cloud/','https://aco.monster']
         # 登录信息
-        self.email = input()
-        self.password = input()
+        self.email = ['vx1999@163.com','vx1999@163.com','liuyq917@gmail.com','liuyq917@gmail.com','liuyq917@gmail.com']
+        self.password = ['Xxkjb2005','QAZwsx123789','amy070712','amy070712','amy070712']
         # Server酱推送（可空）
-        self.sckey = input()
+        self.sckey = 
         # 酷推qq推送（可空）
-        self.ktkey = input()
+        self.ktkey = 
 
     def checkin(self):
         msgall = ''
