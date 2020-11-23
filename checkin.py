12e92a94006d3c396a24b3b78a9933bbd481373b
liuyq917 <38929869+liuyq917@users.noreply.github.com>
2020-11-23 12:57:34 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -1,13 +1,90 @@
-#Time : 2020/9/17 9:52
-#Auth : Ne-21
-#Des : sspanel自动每日签到脚本
-#File :sspanel_qd.py
-#IDE :PyCharm
-#Motto:Another me.
-#sspanel自动每日签到脚本，基于项目https://github.com/zhjc1124/ssr_autocheckin修改
+import requests
+import re
+
+requests.packages.urllib3.disable_warnings()
 
 
+class SspanelQd(object):
+    def __init__(self):
+        # 机场地址
+        self.base_url = ['https://www.paofu.cloud','https://youyun777.net','https://sakura.kozow.com','https://www.paofu.cloud','https://aco.monster']
+        # 登录信息
+        self.email = ['vx1999@163.com','vx1999@163.com','liuyq917@gmail.com','liuyq917@gmail.com','liuyq917@gmail.com']
+        self.password = ['Xxkjb2005','QAZwsx123789','amy070712','amy070712','amy070712']
+        # Server酱推送（可空）
+        self.sckey = input()
+        # 酷推qq推送（可空）
+        self.ktkey = 'e83726bde9932cc5a526e81823879e37'
 
-print(input())
+    def checkin(self):
+        msgall = ''
+        for i in range(len(self.base_url)):
 
+            email = self.email[i].split('@')
+            email = email[0] + '%40' + email[1]
+            password = self.password[i]
+
+            session = requests.session()
+
+            session.get(self.base_url[i], verify=False)
+
+            login_url = self.base_url[i] + '/auth/login'
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            }
+
+            post_data = 'email=' + email + '&passwd=' + password + '&code='
+            post_data = post_data.encode()
+            response = session.post(login_url, post_data, headers=headers, verify=False)
+
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+                'Referer': self.base_url[i] + '/user'
+            }
+
+            response = session.post(self.base_url[i] + '/user/checkin', headers=headers, verify=False)
+            msg = (response.json()).get('msg')
+            
+            msgall = msgall + self.base_url[i] + '\n\n' + msg + '\n\n'
+            print(msg)
+
+            info_url = self.base_url[i] + '/user'
+            response = session.get(info_url, verify=False)
+
+
+        return msgall
+        
+    # Server酱推送
+    def server_send(self, msg):
+        if self.sckey == '':
+            return
+        server_url = "https://sc.ftqq.com/" + str(self.sckey) + ".send"
+        data = {
+                'text': "签到完成，点击查看详细信息~",
+                'desp': msg
+            }
+        requests.post(server_url, data=data)
+
+    # 酷推QQ推送
+    def kt_send(self, msg):
+        if self.ktkey == '':
+            return
+        kt_url = 'https://push.xuthus.cc/send/'+str(self.ktkey)
+        data = ('签到完成，点击查看详细信息~\n'+str(msg)).encode("utf-8")
+        requests.post(kt_url, data=data)
+
+    def main(self):
+        msg = self.checkin()
+        self.server_send(msg)
+        self.kt_send(msg)
+
+# 云函数入口
+def main_handler(event, context):
+    run = SspanelQd()
+    run.main()
+
+if __name__ == '__main__':
+    run = SspanelQd()
+    run.main()
 
